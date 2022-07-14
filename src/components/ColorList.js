94d593bf57a5924ee7a06f63a3c9d637be47de81
fhidesjoseph <fhidesjoseph@gmail.com>
2022-07-14 16:22:32 +0300

    Done.

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,16 +8,31 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
+  const colorElements= colors.map((color)=>{
+    return <li style={{color:color}}>{color}</li>
+  });
+  const users = [
+    { id: 1, firstName: "Duane", lastName: "Watson" },
+    { id: 2, firstName: "Duane", lastName: "Johnson" },
+  ];
+
+  const userHeadings = users.map((user) => {
+    return <h1 key={user.id}>{user.firstName}</h1>;
+  });
+
+  const fib = [0, 1, 1, 2, 3, 5];
+
+const fibList = fib.map((number, index) => {
+  return <div key={index}>{number}</div>;
+});
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+      {colorElements}
+        {userHeadings}
+        {fibList}
       </ol>
     </div>
   );
